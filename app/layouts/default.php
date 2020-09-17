bc7f4bc14af207b5474170e6742ccad02fe83120
Daniel Flynn <daniel.n.flynn@gmail.com>
2020-09-17 10:34:08 -0700

    feat: spruced up the theme a little

--- app/layouts/default.php
+++ app/layouts/default.php
@@ -3,22 +3,53 @@
         <title>Minimal MVC - By Daniel Flynn</title>
         <?php
             $this->styleEnqueue('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
+            $this->styleEnqueue('style', $this->publicUrl('css/style.min.css'), ['bootstrap']);
             
-            $this->scriptRegister('jquery-cdn', 'https://code.jquery.com/jquery-3.5.1.min.jsx');
-            $this->scriptRegister('jquery', 'window.jQuery || document.write(\'<script src="' . $this->publicUrl('/js/jquery-3.5.1.min.js') . '"><\/script>\')', ['jquery-cdn']);
+            $this->scriptRegister('jquery-cdn', 'https://code.jquery.com/jquery-3.5.1.min.js');
+            $this->scriptRegister('jquery', 'window.jQuery || document.write(\'<script src="' . $this->publicUrl('js/jquery-3.5.1.min.js') . '"><\/script>\')', ['jquery-cdn']);
+            $this->scriptRegister('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js', ['jquery']);
+            
+            $this->scriptEnqueue('bootstrap');
 
             $this->outputStyles();
             $this->outputScripts();
         ?>
     </head>
-    <body class="<?php echo $this->bodyClass() ?>">
-        <header>
-            <div class="container">
-                <?php $request = DI::getDefault()->get('Request'); ?>
-                <h1><?php echo $request->getControllerName() ?> - <?php echo $request->getActionName() ?></h1>
-            </div>
+    <body class="<?php echo $this->bodyClass(IS_LOCAL ? 'dev' : '') ?>">
+        <header class="header">
+            <nav class="navbar navbar-expand-lg" role="navigation">
+                <div class="container">
+                    <a class="navbar-brand" href="<?php echo $this->baseUrl(); ?>">
+                        <img src="<?php echo $this->publicUrl('images/logo.png') ?>" class="img-fluid" alt="Minimal MVC">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar icon-bar-x"></span>
+                        <span class="icon-bar icon-bar-x icon-bar-sneaky"></span>
+                        <span class="icon-bar"></span>
+                        <span class="sr-only">Toggle navigation</span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="main-menu">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item <?php echo $this->isRouteActive('Index/Index') ? 'active' : '' ?>">
+                                <a class="nav-link" href="<?php echo $this->baseUrl() ?>">Home</a>
+                            </li>
+                            <li class="nav-item <?php echo $this->isRouteActive('Index/DoesNotExist') ? 'active' : '' ?>">
+                                <a class="nav-link" href="<?php echo $this->baseUrl('Index/DoesNotExist') ?>">Subpage That Doesn't Exist</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </header>
+
         <?php echo $this->getContents() ?>
+
+        <footer class="footer mt-3">
+            <div class="container">
+                <div class="copyright">This is where a copyright could go <a href="#">Team 7</a>.</div>
+            </div>
+        </footer>
         <?php $this->outputScripts('footer') ?>
     </body>
 </html>
--- app/views/Index/Index.php
+++ app/views/Index/Index.php
@@ -1,3 +1,52 @@
-<div class="container">
-    Homepage
-</div>
+<main class="main main-home">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-8">
+                <article class="article">
+                    <h1>Wow!</h1>
+                    <p>wow. such aenean! plz full. wow! txt me. much text. very full! need swag! very layout. very ipsum! oh my word! oh my swag. oh my layout. want dolor. so text! go elit! so swag. such doge. wow. rate me, oh my swag. many amet. many consectetur. i iz cute?, very sit, very layout. wow! much design! yes master doge. wow, wow, such lorem! rate me. wow! many amet. i iz cute?, wow! yes master doge, such doge. such doge. plz doge! such word! such ipsum, yes master doge! very doge. i can haz text! go design! much layout. i can haz text! very doge. very sit,</p>
+                    <p>so adipiscing. yes master doge! very doge. such doge. such ipsum, much text. txt me. need word. such ipsum, oh my layout. much full, oh my layout. such aenean! wow, such word! rate me! plz doge! such lorem! such ipsum, much design! much design! so adipiscing. very full! such lorem! go elit! so swag. plz full. many amet. want dolor. much layout. wow, plz full. many design, such aenean! wow, go design! very ipsum! wow, i can haz text! plz full. oh my word! many consectetur. much text. very ipsum! much text. so text! plz full. so text! such ipsum, such doge. </p>
+                </article>
+            </div>
+            <div class="col-md-4">
+                <h2>Sidebar?!</h2>
+                <p>many design, plz full. i can haz text! very ipsum! much layout. wow. very full! plz full. much full, many design, want dolor. such doge. such aenean! such aenean! plz doge! many design, very ipsum! need word. plz full. such doge. such word! much full, very full! need swag! such word! plz ipsum. very ipsum! many consectetur. so swag. very layout. wow! wow, need word. very full! so adipiscing. need word. wow! plz full. wow, such aenean! plz ipsum. go elit! plz full. many consectetur. need swag! much full, much full, many consectetur. such doge. </p>
+            </div>
+        </div>
+
+        <div class="row mt-3">
+            <div class="col">
+                <div class="card">
+                    <img class="card-img-top" src="https://placeimg.com/640/400/tech" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img class="card-img-top" src="https://placeimg.com/640/400/people" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img class="card-img-top" src="https://placeimg.com/640/400/arch" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-info">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</main>
