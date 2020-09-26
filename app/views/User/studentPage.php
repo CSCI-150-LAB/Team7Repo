7d5caca8a8e0eedaab26d02835a3bb2da183e1ce
sank27 <47614581+sank27@users.noreply.github.com>
2020-09-26 16:50:36 -0700

    Starting student profile

--- app/views/User/studentPage.php
+++ app/views/User/studentPage.php
@@ -1,53 +1,10 @@
 <?php
-    //Needs info from registration page 
-    require 'RegistrationPage.PHP';
-    
-    //Connecting to database 
-    $serverName = "fsu.dandi.dev";                                                                                                 
-    $serverUser = "fluser2020";                                                                                                       
-    $serverPass = "Qd7Qbb2LgQ";                                                                                                           
-    $dbName = "feedbackloop";
-    $conn = new mysqli($serverName, $serverUser, $serverPass, $dbName);
-
-    //Checking to make sure connection was successful to database 
-    if($conn->connect_error)
-    {
-        echo 'Connection error' . $conn->connect_error;
-    }
-
-    //Needs to check if user clicked submitted after registering their credentials 
-    if(isset($_POST['submit']))
-    {
-        //Needs to check if the user clicked student as a role
-        $userRole = $_POST['role'];
-
-        //If user is a student then register and assign their credentials for database 
-        if($userRole == "student")
-        {
-            $firstName = $_POST['first'];
-            $lastName = $_POST['last'];
-            $useremail = $_POST['email'];
-            $userPassword = $_POST['pass'];
-            $userId = $_POST['id'];
-        }
-
-        //Binding new student parameters into database 
-        $sql = "INSERT INTO users (id, email, first_name, last_name password, type) VALUES (?, ?, ?, ?, ?, ?)";
-        $stmt = mysqli_stmt_init($conn);
-
-                if(!mysqli_prepare($stmt, $sql))
-                {
-                    echo 'Error';
-                    exit();
-                }
-                else 
-                {
-                    mysqli_stmt_bind_param($stmt, "isssss", $userId, $useremail, $firstName, $lastName, $userPassword, $userRole); //May need to revise because of issss? 
-                }
-
-        
-    }
-
+session_start();
+ //Student information needs to be passed after registration and login page 
+ //Not sure if this is correct 
+   $profileFirstName = $_POST['first'];
+   $profileLastName = $_POST['last'];
+   $profileID = $_POST['id'];
 
 ?>
 
@@ -60,5 +17,19 @@
 </head>
 <body>
     <h1>You are in the student profile</h1>
+    <?php echo 'Hello' + $profileFirstName + $profileLastName?>; <!--Output Student first and last name -->
+
+    <br>
+    <h2>Preferred Learning Styles</h2>
+    <input type="radio" name="style" value="visual">
+    <label for="visual">Visual</label>
+    <input type="radio" name="style" value="audio">
+    <label for="audio">Audio</label>
+    <input type="radio" name="style" value="kinesthetic">
+    <label for="kinesthetic">Kinesthetic</label>
+    <input type="radio" name="style" value="reading/writing">
+    <label for="reading/writing">Reading/Writing</label>
+
+
 </body>
 </html>
