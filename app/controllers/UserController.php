77df3e35da84cbf7aed58f903ab1001bb3a1e311
sank27 <47614581+sank27@users.noreply.github.com>
2020-10-01 13:38:28 -0700

    After user registration or user login, it will redirect to student profile. Still need to compare whether user is student or instructor

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller {
 
 			if ($user) { //Logs in user 
 				User::loginUser($user);
-				return $this->redirect($this->viewHelpers->baseUrl('/User/Profile/' . $user->id));
+				return $this->redirect($this->viewHelpers->baseUrl('/Student/Profile/' . $user->id));
 			}
 			else { //User was not found or email/password was mistyped
 				$errors[] = 'Email or password invalid';
@@ -54,7 +54,7 @@ class UserController extends Controller {
 				if ($user->save()) {
 					User::loginUser($user);
 
-					return $this->redirect($this->viewHelpers->baseUrl("/User/Profile/{$user->id}")); 
+					return $this->redirect($this->viewHelpers->baseUrl("/Student/Profile/{$user->id}")); 
 				}
 				else {
 					$errors[] = 'Failed to save the profile';
