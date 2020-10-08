666b39a14a2b9323a0bf39b476be709a02dfc1bf
sank27 <47614581+sank27@users.noreply.github.com>
2020-10-07 20:18:53 -0700

    Added little more styling to student profile

--- app/views/Student/Profile.php
+++ app/views/Student/Profile.php
@@ -1,11 +1,14 @@
+<h1 class="mb-3" style= "background-color: #13284c; padding:60px; color: #ffffff;">Welcome to <?php  echo $user->firstName." ".$user->lastName?>
+profile!
+</h1><br>
+
+<img src="<?php echo $this->publicUrl('images/blank_avatar.png')?>" width="200px" alt="blank_avatar">
+
 <?php
     $profile = StudentModel::getByKey($user->id);
     $currentUser = User::getCurrentUser();
-    echo "<h1 class = 'sprofile'>Welcome to ";
-    echo $user->firstName." ".$user->lastName." profile!</h1><br>";
-    //Makes heading of student profile, with title if chosen
     if($user->id == $currentUser->id) {
-		echo "<a href='" . $this->baseUrl('/Student/ProfileEdit/' . $user->id) . "'>Edit Profile</a>";
+		echo "<a class = 'btn btn-secondary float-right' style='color: #ffffff;' href='" . $this->baseUrl('/Student/ProfileEdit/' . $user->id) . "'>Edit Profile</a>";
     } //Allows user to edit profile if current profile is the user's profile
 
     echo    "<h3 class = 'sprofile'>$user->email</h3><br>";
