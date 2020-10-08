69caf42e430eed5616aa29cfd9d800418c8e76b7
wongrob <70239645+wongrob@users.noreply.github.com>
2020-10-08 12:53:34 -0700

    improved edit profile UI

--- app/views/Instructor/EditProfile.php
+++ app/views/Instructor/EditProfile.php
@@ -1,129 +1,204 @@
 <h1>Edit Your Profile</h1>
 <form method = 'POST'>
     <!--Once submitted, save to database by sending to controller-->
-    Department:
-    <input type = 'text' name = 'department' <?php echo "value = '".$profile->department."'"; ?> >
+    <div class="form-group">
+		<label for="department">Department:</label>
+		<input type="text" class="form-control <?php echo !empty($errors['class']) ? 'is-invalid' : '' ?>" id="department" name="department" placeholder="Math" <?php echo "value = '".$profile->department."'"; ?>>
+		<?php if (!empty($errors['department'])) : ?>
+			<div class="invalid-feedback">
+				<?php echo $errors['department'] ?>
+			</div>
+		<?php endif; ?>
+	</div>
     <br>
     <!--Input for user's department-->
     Preferred Title:
     <!--Selection of user's preferred title, autoselects if previously chosen-->
-    <input type = 'radio' name = 'name' value = 'Dr.'
-    <?php   if($profile->name == 'Dr.') {
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='name1' name = 'name' value = 'Dr.'
+            <?php   if($profile->name == 'Dr.') {
                 echo "checked = 'checked'";
             } ?>
-    > Dr.
-    <input type = 'radio' name = 'name' value = 'Professor'
-    <?php   if($profile->name == 'Professor') {
+    >
+        <label class="form-check-label" for='name1'>Dr.</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='name2' name = 'name' value = 'Professor'
+            <?php   if($profile->name == 'Professor') {
                 echo "checked = 'checked'";
             } ?>
-    > Professor
-    <input type = 'radio' name = 'name' value = 'Mr.'
-    <?php   if($profile->name == 'Mr.') {
+    >
+        <label class="form-check-label" for='name2'>Professor</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='name3' name = 'name' value = 'Mr.'
+            <?php   if($profile->name == 'Mr.') {
                 echo "checked = 'checked'";
             } ?>
-    > Mr.
-    <input type = 'radio' name = 'name' value = 'Mrs.'
-    <?php   if($profile->name == 'Mrs.') {
+    >
+        <label class="form-check-label" for='name3'>Mr.</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='name4' name = 'name' value = 'Mrs.'
+            <?php   if($profile->name == 'Mr2.') {
                 echo "checked = 'checked'";
             } ?>
-    > Mrs.
-    <input type = 'radio' name = 'name' value = 'Ms.'
-    <?php   if($profile->name == 'Ms.') {
+    >
+        <label class="form-check-label" for='name4'>Mrs.</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='name5' name = 'name' value = 'Ms.'
+            <?php   if($profile->name == 'Ms.') {
                 echo "checked = 'checked'";
             } ?>
-    > Ms.
+    >
+        <label class="form-check-label" for='name5'>Ms.</label>
+    </div>
+    <br>
     <br>
 	Visual Style Usage:
     <!--Selection of user's visual style usage, autoselects if previously chosen-->
-    <input type = 'radio' name = 'visual' value = 'Primarily'
-    <?php   if($profile->visual == 'primarily') {
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='visual1' name = 'visual' value = 'Primarily'
+            <?php   if($profile->visual == 'primarily') {
                 echo "checked = 'checked'";
             } ?>
-    > Primarily
-    <input type = 'radio' name = 'visual' value = 'Somewhat'
-    <?php   if($profile->visual == 'somewhat') {
+    >
+        <label class="form-check-label" for='visual1'>Primarily</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='visual2' name = 'visual' value = 'Somewhat'
+            <?php   if($profile->visual == 'somewhat') {
                 echo "checked = 'checked'";
             } ?>
-    > Somewhat
-    <input type = 'radio' name = 'visual' value = 'Minimal'
-    <?php   if($profile->visual == 'minimal') {
+    >
+        <label class="form-check-label" for='visual2'>Somewhat</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='visual3' ame = 'visual' value = 'Minimal'
+            <?php   if($profile->visual == 'minimal') {
                 echo "checked = 'checked'";
             } ?>
-    > Minimal
-    <input type = 'radio' name = 'visual' value = 'Not at all'
-    <?php   if($profile->visual == 'not at all') {
+    >
+        <label class="form-check-label" for='visual3'>Minimal</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='visual4' name = 'visual' value = 'Not at all'
+            <?php   if($profile->visual == 'not at all') {
                 echo "checked = 'checked'";
             } ?>
-    > Not at all
+    >
+        <label class="form-check-label" for='visual4'>Not at all</label>
+    </div>
+    <br>
     <br>
 	Auditory Style Usage:
     <!--Selection of user's auditory style usage, autoselects if previously chosen-->
-    <input type = 'radio' name = 'auditory' value = 'Primarily'
-    <?php   if($profile->auditory == 'primarily') {
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='auditory1' name = 'auditory' value = 'Primarily'
+            <?php   if($profile->auditory == 'primarily') {
                 echo "checked = 'checked'";
             } ?>
-    > Primarily
-    <input type = 'radio' name = 'auditory' value = 'Somewhat'
-    <?php   if($profile->auditory == 'somewhat') {
+    >
+        <label class="form-check-label" for='auditory1'>Primarily</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='auditory2' name = 'auditory' value = 'Somewhat'
+            <?php   if($profile->auditory == 'somewhat') {
                 echo "checked = 'checked'";
             } ?>
-    > Somewhat
-    <input type = 'radio' name = 'auditory' value = 'Minimal'
-    <?php   if($profile->visual == 'minimal') {
+    >
+        <label class="form-check-label" for='auditory2'>Somewhat</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='auditory3' name = 'auditory' value = 'Minimal'
+            <?php   if($profile->auditory == 'minimal') {
                 echo "checked = 'checked'";
             } ?>
-    > Minimal
-    <input type = 'radio' name = 'auditory' value = 'Not at all'
-    <?php   if($profile->auditory == 'not at all') {
+    >
+        <label class="form-check-label" for='auditory4'>Minimal</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='auditory4' name = 'auditory' value = 'Not at all'
+            <?php   if($profile->auditory == 'not at all') {
                 echo "checked = 'checked'";
             } ?>
-    > Not at all
+    >
+        <label class="form-check-label" for='auditory3'>Not at all</label>
+    </div>
+    <br>
     <br>
 	Reading and Writing Style Usage:
     <!--Selection of user's reading/writing style usage, autoselects if previously chosen-->
-    <input type = 'radio' name = 'readwrite' value = 'Primarily'
-    <?php   if($profile->readwrite == 'primarily') {
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='readwrite1' name = 'readwrite' value = 'Primarily'
+            <?php   if($profile->readwrite == 'primarily') {
                 echo "checked = 'checked'";
             } ?>
-    > Primarily
-    <input type = 'radio' name = 'readwrite' value = 'Somewhat'
-    <?php   if($profile->readwrite == 'somewhat') {
+    >
+        <label class="form-check-label" for='readwrite1'>Primarily</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='readwrite2' name = 'readwrite' value = 'Somewhat'
+            <?php   if($profile->readwrite == 'somewhat') {
                 echo "checked = 'checked'";
             } ?>
-    > Somewhat
-    <input type = 'radio' name = 'readwrite' value = 'Minimal'
-    <?php   if($profile->readwrite == 'minimal') {
+    >
+        <label class="form-check-label" for='readwrite2'>Somewhat</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='readwrite3' name = 'readwrite' value = 'Minimal'
+            <?php   if($profile->readwrite == 'minimal') {
                 echo "checked = 'checked'";
             } ?>
-    > Minimal
-    <input type = 'radio' name = 'readwrite' value = 'Not at all'
-    <?php   if($profile->readwrite == 'not at all') {
+    >
+        <label class="form-check-label" for='readwrite3'>Minimal</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='readwrite4' name = 'readwrite' value = 'Not at all'
+            <?php   if($profile->readwrite == 'not at all') {
                 echo "checked = 'checked'";
             } ?>
-    > Not at all
+    >
+        <label class="form-check-label" for='readwrite4'>Not at all</label>
+    </div>
+    <br>
     <br>
     Kinesthetic Style Usage:
     <!--Selection of user's kinesthetic style usage, autoselects if previously chosen-->
-    <input type = 'radio' name = 'kines' value = 'Primarily'
-    <?php   if($profile->kines == 'primarily') {
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='kines' name = 'kines' value = 'Primarily'
+            <?php   if($profile->kines == 'primarily') {
                 echo "checked = 'checked'";
             } ?>
-    > Primarily
-    <input type = 'radio' name = 'kines' value = 'Somewhat'
-    <?php   if($profile->kines == 'somewhat') {
+    >
+        <label class="form-check-label" for='kines1'>Primarily</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='kines2' name = 'kines' value = 'Somewhat'
+            <?php   if($profile->kines == 'somewhat') {
                 echo "checked = 'checked'";
             } ?>
-    > Somewhat
-    <input type = 'radio' name = 'kines' value = 'Minimal'
-    <?php   if($profile->kines == 'minimal') {
+    >
+        <label class="form-check-label" for='kines2'>Somewhat</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='kines3' name = 'kines' value = 'Minimal'
+            <?php   if($profile->kines == 'minimal') {
                 echo "checked = 'checked'";
             } ?>
-    > Minimal
-    <input type = 'radio' name = 'kines' value = 'Not at all'
-    <?php   if($profile->kines == 'not at all') {
+    >
+        <label class="form-check-label" for='kines3'>Minimal</label>
+    </div>
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" id='kines4' name = 'kines' value = 'Not at all'
+            <?php   if($profile->kines == 'not at all') {
                 echo "checked = 'checked'";
             } ?>
-    > Not at all
+    >
+        <label class="form-check-label" for='kines4'>Not at all</label>
+    </div>
 	<br>
-	<input type = 'submit' name = 'save changes' value = 'Save Changes'>
+    <br>
+	<input type = 'submit' class = 'btn btn-primary' name = 'save changes' value = 'Save Changes'>
 </form> <!--Submit button-->
