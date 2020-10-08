4b281f7f764337cf8405b9ba493fb3a43d672b87
wongrob <70239645+wongrob@users.noreply.github.com>
2020-10-08 12:08:26 -0700

    made register UI slightly better

--- app/views/User/Register.php
+++ app/views/User/Register.php
@@ -12,37 +12,39 @@
 	<div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputfirst">First Name</label>
-	  <input type="text" name = "first" class="form-control" id="inputfirst" placeholder="First">
-	  <!--Input for first name is text entry stored as 'first'-->
 	  <span style="color:red">* <?php echo $firstError; ?> </span>
 	  <!--Output the appropriate error message (if there is one)-->
+	  <input type="text" name = "first" class="form-control" id="inputfirst" placeholder="First">
+	  <!--Input for first name is text entry stored as 'first'-->
     </div>
     <div class="form-group col-md-6">
       <label for="inputlast">Last Name</label>
-	  <input type="text" name = "last" class="form-control" id="inputlast" placeholder="Last">
-	  <!--Input for last name is text entry stored as 'last'-->
 	  <span style="color:red">* <?php echo $lastError; ?> </span>
 	  <!--Output the appropriate error message (if there is one)-->
+	  <input type="text" name = "last" class="form-control" id="inputlast" placeholder="Last">
+	  <!--Input for last name is text entry stored as 'last'-->
     </div>
   </div>
   <div class="form-group">
     <label for="inputemail">Email</label>
-    <input type="text" name = "email" class="form-control" id="inputemail" placeholder="user@csufresno.edu">
-	<!--Input for email is text entry stored as 'email'-->
 	<span style="color:red">* <?php echo $emailError; ?> </span>
 	<!--Output the appropriate error message (if there is one)-->
+    <input type="text" name = "email" class="form-control" id="inputemail" placeholder="user@csufresno.edu">
+	<!--Input for email is text entry stored as 'email'-->
   </div>
   <div class="form-group">
 	<!--Input for password is of type password stored as 'pass'-->
     <label for="inputpass">Password</label>
-	<input type="password" name = "pass" class="form-control" id="inputpass">
-	<!--Type password allows for the password to be hidden-->
 	<span style="color:red">* <?php echo $passError; ?> </span>
 	<!--Output the appropriate error message (if there is one)-->
+	<input type="password" name = "pass" class="form-control" id="inputpass">
+	<!--Type password allows for the password to be hidden-->
   </div>		
 
 	<br>
-	Role:
+	Role
+	<span style="color:red">* <?php echo $roleError; ?> </span>
+	<!--Output the appropriate error message (if there is one)-->:
 	<!--Input for role is a radio option stored as 'role'-->
 	<div class="form-check form-check-inline">
     <input class="form-check-input" type="radio" name="role" id="inlineRadio1" value="student">
@@ -58,8 +60,6 @@
     <input class="form-check-input" type="radio" name="role" id="inlineRadio3" value="admin">
 	<label class="form-check-label" for="inlineRadio3">Admin</label>
 	<!--The last option is to set the role value to 'admin'-->
-	<span style="color:red">* <?php echo $roleError; ?> </span>
-	<!--Output the appropriate error message (if there is one)-->
     </div>
 
 
