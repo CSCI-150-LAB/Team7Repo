077fb0c4063caae51e3cb4162dc4325c86bc2423
wongrob <70239645+wongrob@users.noreply.github.com>
2020-10-08 12:19:47 -0700

    edited add student layout

--- app/views/Instructor/AddStudent.php
+++ app/views/Instructor/AddStudent.php
@@ -3,9 +3,15 @@ var_dump($errors);
         $class = InstructorClasses::find("classid =:0:", $classid); ?>
 <h1>Add a New Student to <?php echo $class[0]->class ?></h1>
 <form method = 'POST'>
-    <!--Once submitted, save to database by sending to controller-->
-    Student Email:
-    <input type = 'text' name = 'email'>
+    <div class = 'form-group'>
+		<label for = 'email'>Student Email:</label>
+		<input type = 'text' class = "form-control <?php echo !empty($errors['class']) ? 'is-invalid' : '' ?>" id = 'email' name = 'email' placeholder = 'student@mail.fresnostate.edu'>
+		<?php if (!empty($errors['email'])) : ?>
+			<div class = 'invalid-feedback'>
+				<?php echo $errors['email'] ?>
+			</div>
+		<?php endif; ?>
+	</div>
     <br>
-	<input type = 'submit' name = 'add student' value = 'Add Student'>
+	<input type = 'submit' class = 'btn btn-primary' name = 'add student' value = 'Add Student'>
 </form> <!--Submit button-->
