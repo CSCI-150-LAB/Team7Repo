d2196491c2122a93e61b368e61182727f5990d06
SumanjitGi11 <70239811+SumanjitGi11@users.noreply.github.com>
2020-10-14 15:26:49 -0700

    Create InstructorRatings.php
    Model for Recommendations Table

---
+++ app/models/InstructorRatings.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @Table('instructorratings')
+ */
+class InstructorRatings extends Model {
+
+	/**
+	 * @Key
+	 * @Column('rating_id')
+	 */
+	public $ratingId;
+
+	/**
+	 * @Column('rating')
+	 */
+    public $rating;
+    
+    /**
+     * @Column('recommendation')
+     */
+    public $recommendation;
+
+    /**
+     * @Column('author_id')
+     */
+    public $authorId;
+
+    /**
+     * @Column('instructor_id')
+     */
+    public $instructorId;
+
+}
