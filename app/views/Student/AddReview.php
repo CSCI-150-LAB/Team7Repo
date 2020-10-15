0c90b2c3291416996b0fc4ae1c86567aed1af585
SumanjitGi11 <70239811+SumanjitGi11@users.noreply.github.com>
2020-10-15 10:02:30 -0700

    Fix Rating to Store Data

--- app/views/Student/AddReview.php
+++ app/views/Student/AddReview.php
@@ -3,11 +3,11 @@
 <form method = 'POST'>
 
 Rating (Stars):
-    <input type = 'radio' name = 'rating'> 1
-    <input type = 'radio' name = 'rating'> 2
-    <input type = 'radio' name = 'rating'> 3
-    <input type = 'radio' name = 'rating'> 4
-    <input type = 'radio' name = 'rating'> 5
+    <input type = 'radio' name = 'rating' value = 1> 1
+    <input type = 'radio' name = 'rating' value = 2> 2
+    <input type = 'radio' name = 'rating' value = 3> 3
+    <input type = 'radio' name = 'rating' value = 4> 4
+    <input type = 'radio' name = 'rating' value = 5> 5
     <br>
 
     Recommendation:
