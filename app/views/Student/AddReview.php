89c55b210dd7c102e9f664d8a771e4b62c43807a
SumanjitGi11 <70239811+SumanjitGi11@users.noreply.github.com>
2020-10-20 20:55:33 -0700

    Changing UI
    Incomplete UI change

--- app/views/Student/AddReview.php
+++ app/views/Student/AddReview.php
@@ -2,22 +2,28 @@
 
 <form method = 'POST'>
 
-Rating (Stars):
+    Rating (Stars):
     <input type = 'radio' name = 'rating' value = 1> 1
     <input type = 'radio' name = 'rating' value = 2> 2
     <input type = 'radio' name = 'rating' value = 3> 3
     <input type = 'radio' name = 'rating' value = 4> 4
     <input type = 'radio' name = 'rating' value = 5> 5
     <br>
+    <br>
 
     Recommendation:
-    <input type = 'text' name = 'recommendation' placeholder = "Tell us about your experiences with this instructor.">
+    <br>
+    <div class="form-group">
+    <textarea class="form-control" name = 'recommendation' placeholder="Tell us about your experiences with this instructor." rows="3"></textarea>
+    </div>
     <br>
 
     Would you like this rating to be anonymous?
     <input type = 'radio' name = 'anon' value = 'yes'> Yes
     <input type = 'radio' name = 'anon' value = 'no'> No
 
+    <br>
+    <br>
     <br>
     <button type="submit" class="btn btn-primary">Add Review</button>
 
