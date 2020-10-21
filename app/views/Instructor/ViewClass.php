7d53497fcee6c02cb26024dd242cd2e9c6d1ea7b
Daniel Flynn <daniel.n.flynn@gmail.com>
2020-10-20 19:33:36 -0700

    feat: added class id to link

--- app/views/Instructor/ViewClass.php
+++ app/views/Instructor/ViewClass.php
@@ -1,7 +1,7 @@
 <h1> <?php echo $class->class." ".$class->getClassTimeString(); ?> </h1>
 <h2> <?php echo $class->description; ?> </h2>
 <a class = "btn btn-secondary float-right" style="color: #ffffff;" href ='<?php echo $this->baseUrl("/Instructor/AddStudent/{$class->classid}") ?>'>Add a Student</a><br>
-<a class = "btn btn-secondary" style="color: #ffffff;" href ='<?php echo $this->baseUrl("/Feedback/InitiateFeedback") ?>'>Create feedback session</a><br>
+<a class = "btn btn-secondary" style="color: #ffffff;" href ='<?php echo $this->baseUrl("/Feedback/InitiateFeedback/{$class->classid}") ?>'>Create feedback session</a><br>
 <a class = "btn btn-secondary float-right" style="color: #ffffff;" href ='<?php echo $this->baseUrl("/Feedback/ViewSessions") ?>'>View feedback sessions</a><br>
 <?php $studentids = studentClasses::find("classId =:0:", $class->classid);?>
 <table class="table table-bordered">
