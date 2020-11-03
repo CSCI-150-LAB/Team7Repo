1160e515fcf39cb66e3be3bbc6c670c00face490
Karla G. Moreno <morenokarla@mail.fresnostate.edu>
2020-11-03 06:18:02 -0800

    Untracked file
    commit to fix issue

---
+++ app/views/Admin/Panel.php
@@ -0,0 +1,31 @@
+<div class="container">
+<div class="row mt-3 ">
+    <div class="col">
+        <div class="card">
+            <img class="card-img-top" src="<?php echo $this->publicUrl('images/vicon.png')?>" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title" style="text-align: center">Visual</h5>
+                <p class="card-text">This learning style is where students best learn by seeing visual aids such as images, video and diagrams.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <img class="card-img-top" src="<?php echo $this->publicUrl('images/aicon.png')?>" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title" style="text-align: center">Auditory </h5>
+                <p class="card-text">This learning style is where students best learn listening to presentations or audio from a recorded lecture.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <img class="card-img-top" src="<?php echo $this->publicUrl('images/rwicon.png')?>" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title" style="text-align: center">Reading/Writing </h5>
+                <p class="card-text">This learning style is where students best learn by reading information or writing notes or summaries.</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
