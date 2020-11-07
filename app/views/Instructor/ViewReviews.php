01f0570ae51890c4b2d5ef0fb044c273b62a2688
wongrob <70239645+wongrob@users.noreply.github.com>
2020-11-06 18:46:59 -0800

    fixed issue #47

--- app/views/Instructor/ViewReviews.php
+++ app/views/Instructor/ViewReviews.php
@@ -1,22 +1,23 @@
-<?php
-    $instructorInfo = User::find("id = :0:", $instructor->instructorid);
-?>
-
 <h1 class="mb-3" style= "background-color: #13284c; padding:60px; color: #ffffff;">
-Reviews for <?php  echo $instructor->name . " " . $instructorInfo[0]->firstName . " " . $instructorInfo[0]->lastName?></h1>
+Reviews for <?php  
+$instructorInfo = InstructorModel::findOne("id = :0:", $instructor->id);
+if($instructorInfo) {
+    echo $instructorInfo->name . " ";
+}
+    echo $instructor->firstName . " " . $instructor->lastName?></h1>
 
 <b>Note: </b> <i class="fas fa-check" style="color:green;"></i> next to a reviewer name confirms they have been enrolled in a course taught by this instructor.
 
 <?php   $currentUser = User::getCurrentUser();
         $isStud = User::find("id = :0:", $currentUser->id);
         if ($isStud[0]->type == 'student') {
-            echo "<a class = 'btn btn-secondary float-right' style='color: #ffffff;' href = '".$this->baseUrl("/Student/AddReview/{$instructor->instructorid}")."'>Add Review</a><br>";
+            echo "<a class = 'btn btn-secondary float-right' style='color: #ffffff;' href = '".$this->baseUrl("/Student/AddReview/{$instructor->id}")."'>Add Review</a><br>";
         } //If the user is a student, ratings may be added
 ?>
 
 <br>
 
-<?php $reviews = InstructorRatings::find("instructor_id = :0:", $instructor->instructorid);?>
+<?php $reviews = InstructorRatings::find("instructor_id = :0:", $instructor->id);?>
 
 <?php 
     for ($counter = count($reviews)-1; $counter >= 0; $counter--) { ?>
