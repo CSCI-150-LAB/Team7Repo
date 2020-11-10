5f6f43b445b9f987a818ba09ceb03376c637f7f8
sank27 <47614581+sank27@users.noreply.github.com>
2020-11-10 10:41:11 -0800

    Spaced out button from text box

--- app/views/Feedback/Response.php
+++ app/views/Feedback/Response.php
@@ -7,11 +7,11 @@
 <h2 class="mb-3" style= "background-color: #13284c; padding:60px; color: #ffffff;">Response</h2>
 
 
+<h3 style="text-align:center;">Text feedback Submission</h3>
 
-
-<form method="POST">
+<form method="POST" style="text-align:center;">
 
     <textarea id="student-response" name="response" rows="5" cols="80"></textarea> <br>
-    <input type="submit" id="sub" value="Submit" class = "btn btn-secondary" style="color: #ffffff ;">
+    <input type="submit" id="sub" value="Submit" class = "btn btn-secondary" style="color: #ffffff; margin-top: 30px;">
 
 </form>
