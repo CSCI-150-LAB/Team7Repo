110605446094f3f35d33f90520b2b3b459c168aa
Daniel Flynn <daniel.n.flynn@gmail.com>
2020-11-19 16:13:35 -0800

    fixes Instructor Profile Ratings #61

--- app/views/Student/AddReview.php
+++ app/views/Student/AddReview.php
@@ -1,25 +1,24 @@
 <h1>Add Review</h1>
 <div class = "card">
-    <div class = "card-body">    
+    <div class = "card-body">
         <form method = 'POST'>
-            <b>Rating (Stars):</b>
+            <b>Rating:</b>
     
-            <div class = "rating"> 
-           
-                <input id = "fiveStar" type = 'radio' name = 'rating' value = 5 class = "radio-btn hide"> 
-                <label for = "fiveStar"> ☆ </label>
-
-                <input id = "fourStar" type = 'radio' name = 'rating' value = 4 class = "radio-btn hide"> 
-                <label for = "fourStar"> ☆ </label>
-
-                <input id = "threeStar" type = 'radio' name = 'rating' value = 3 class = "radio-btn hide"> 
-                <label for = "threeStar"> ☆ </label>
-
-                <input id = "twoStar" type = 'radio' name = 'rating' value = 2 class = "radio-btn hide">  
-                <label for = "twoStar"> ☆ </label>
-
-                <input id = "oneStar" type = 'radio' name = 'rating' value = 1 class = "radio-btn hide"> 
-                <label for = "oneStar"> ☆ </label>
+            <div class = "rating">
+				<label><?php echo $label ?></label>
+				<div class="rating-field-container">
+					<div class="rating-field">
+						<input type="hidden" name="rating">
+						<?php for ($i = 1; $i <= 5; $i++) : ?>
+						<i class="far fa-star score-<?php echo $i ?>"></i>
+						<?php endfor; ?>
+					</div>
+					<?php if (!empty($errors['rating'])) : ?>
+						<div class="invalid-feedback">
+							<?php echo $errors['rating'] ?>
+						</div>
+					<?php endif; ?>
+				</div>
 
             </div>
 
@@ -57,9 +56,9 @@
             <b>Your grade in the class:</b>
             <input type = 'radio' name = 'grade' value = 'A'> A
             <input type = 'radio' name = 'grade' value = 'B'> B
-            <input type = 'radio' name = 'grade' value = 'A'> C
-            <input type = 'radio' name = 'grade' value = 'B'> D
-            <input type = 'radio' name = 'grade' value = 'A'> F
+            <input type = 'radio' name = 'grade' value = 'C'> C
+            <input type = 'radio' name = 'grade' value = 'D'> D
+            <input type = 'radio' name = 'grade' value = 'F'> F
 
             <br>
             <br>
