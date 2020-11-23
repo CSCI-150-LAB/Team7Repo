c55d477844a7da3d9891fb09eef52971f034f179
sank27 <47614581+sank27@users.noreply.github.com>
2020-11-23 10:10:15 -0800

    switched UI theme for student profile

--- app/views/Student/Profile.php
+++ app/views/Student/Profile.php
@@ -1,166 +1,95 @@
 
-
 <?php
     $profile = StudentModel::getByKey($user->id);
     $currentUser = User::getCurrentUser();
-    /* if($user->id == $currentUser->id) {
+    if($user->id == $currentUser->id) {
 		echo "<a class = 'btn btn-secondary float-right' style='color: #ffffff;' href='" . $this->baseUrl('/Student/ProfileEdit/' . $user->id) . "'>Edit Profile</a>";
-    } */ //Allows user to edit profile if current profile is the user's profile
+    } //Allows user to edit profile if current profile is the user's profile
 
             
 ?>
 
+<!-- <img src="<?php echo $this->publicUrl('images/blank_avatar.png')?>" alt="blank_avatar"/> -->
 
+<body id="profile-style">
+  
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
-
-
-
-<body id="testing">
-    
-
-        <div class="container emp-profile">
-                    <form method="post">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="profile-img">
-                                    <img src="<?php echo $this->publicUrl('images/blank_avatar.png')?>" alt="blank_avatar"/>
-                                    <div class="file btn btn-lg btn-primary">
-                                        Change Photo
-                                        <input type="file" name="file"/>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="profile-head">
-                                            <h5>
-                                                <?php echo $currentUser->getFullName() ?>
-                                            </h5>
-                                            <h6>
-                                                <?php echo $profile->studentMajor ?>
-                                            </h6>
-                                            <!-- <p class="proile-rating">RANKINGS : <span>8/10</span></p> -->
-                                    <ul class="nav nav-tabs" id="myTab" role="tablist">
-                                        <li class="nav-item">
-                                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-md-2">
-                                <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Edit Profile"/>
-                            </div>
+    <div class="container">
+        <div class="main-body">
+        
+              <div class="row gutters-sm">
+                <div class="col-md-4 mb-3">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="d-flex flex-column align-items-center text-center">
+                        <img src="<?php echo $this->publicUrl('images/blank_avatar.png')?>" alt="pic" class="rounded-circle" width="150">
+                        <div class="mt-3">
+                          <h4><?php echo $currentUser->getFullName()?></h4>
+                          <p class="text-secondary mb-1"><?php echo $profile->studentMajor?></p>
                         </div>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="profile-work">
-                                    <p>WORK LINK</p>
-                                    <a href="">Website Link</a><br/>
-                                    <a href="">Bootsnipp Profile</a><br/>
-                                    <a href="">Bootply Profile</a>
-                                    <p>SKILLS</p>
-                                    <a href="">Web Designer</a><br/>
-                                    <a href="">Web Developer</a><br/>
-                                    <a href="">WordPress</a><br/>
-                                    <a href="">WooCommerce</a><br/>
-                                    <a href="">PHP, .Net</a><br/>
-                                </div>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="tab-content profile-tab" id="myTabContent">
-                                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <label>User Id</label>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <p><?php echo $currentUser->id?></p>
-                                                    </div>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <label>Name</label>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <p><?php echo $currentUser->getFullName()?></p>
-                                                    </div>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <label>Email</label>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <p><?php echo $currentUser->email ?></p>
-                                                    </div>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <label>Preferred Learning Style</label>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <p><?php echo $profile->learningStyle?></p>
-                                                    </div>
-                                                </div>
-                                                
-                                    </div>
-                                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <label>Experience</label>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <p>Expert</p>
-                                                    </div>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <label>Hourly Rate</label>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <p>10$/hr</p>
-                                                    </div>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <label>Total Projects</label>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <p>230</p>
-                                                    </div>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <label>English Level</label>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <p>Expert</p>
-                                                    </div>
-                                                </div>
-                                                <div class="row">
-                                                    <div class="col-md-6">
-                                                        <label>Availability</label>
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <p>6 months</p>
-                                                    </div>
-                                                </div>
-                                        <div class="row">
-                                            <div class="col-md-12">
-                                                <label>Your Bio</label><br/>
-                                                <p>Your detail description</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>           
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card mt-3">
+                    <ul class="list-group list-group-flush">
+                      <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                        <h6 class="mb-0">Full Name</h6>
+                        <span class="text-secondary"><?php echo $currentUser->getFullName()?></span>
+                      </li>
+                      <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                        <h6 class="mb-0">Email</h6>
+                        <span class="text-secondary"><?php echo $currentUser->email?></span>
+                      </li>
+                      <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                        <h6 class="mb-0">Preferred Learning Style</h6>
+                        <span class="text-secondary"><?php echo $profile->learningStyle?></span>
+                      </li>
+                      
+                    </ul>
+                  </div>
                 </div>
+                <div class="col-md-8">
+                  <div class="card mb-3">
+                    <div class="card-body">
+                      <div class="row">
+                        <div class="col-sm-3">
+                          <h6 class="mb-0">My Bio</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                         <input type="textarea" width="100px" height="150px">
+                        </div>
+                      </div>
+                     
+                    </div>
+                  </div>
+                  <div class="row gutters-sm">
+                    <div class="col-sm-6 mb-3">
+                      <div class="card h-100">
+                        <div class="card-body" >
+                          <h6 class="d-flex align-items-center mb-3">Other Learning Styles</h6>
+                          <small>Visual</small>
+                          <div class="progress mb-3" style="height: 5px">
+                            <div class="progress-bar bg-primary" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                          <small>Kinesthetic</small>
+                          <div class="progress mb-3" style="height: 5px">
+                            <div class="progress-bar bg-primary" role="progressbar" style="width: 72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                          <small>Audio</small>
+                          <div class="progress mb-3" style="height: 5px">
+                            <div class="progress-bar bg-primary" role="progressbar" style="width: 89%" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                          <small>Reading / Writing</small>
+                          <div class="progress mb-3" style="height: 5px">
+                            <div class="progress-bar bg-primary" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
 </body>
