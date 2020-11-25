825b458e7e467977be5dc38b7de1fa9075c1e7b7
Karla G. Moreno <morenokarla@mail.fresnostate.edu>
2020-11-25 11:40:14 -0800

    feat: select search on

--- app/views/Admin/StartSession.php
+++ app/views/Admin/StartSession.php
@@ -1,13 +1,7 @@
-<?php// Material Select Initialization
-$(document).ready(function() {
-$('.mdb-select').materialSelect();
-}); ?>
-
-
 <div class="container">
     <h1 class="mb-3" style= "background-color: #13284c; padding:60px; color: #ffffff;"> Start Feedback Session</h1>
     <!--Select Class-->
-    <select class="mdb-select md-form colorful-select dropdown-primary">
+    <select data-live-search="true class="mdb-select md-form colorful-select dropdown-primary">
     <?php $count = 1;
         foreach($inclasses as $inclass):?>    
             <option value="<?php echo $count?>">Class ID: <?php echo $inclass->class_id?> Class Title: <?php echo $inclass->class_description?></option>
