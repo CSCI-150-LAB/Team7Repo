9e533fda889e665289077066e3a26d1514031b63
sank27 <47614581+sank27@users.noreply.github.com>
2020-12-01 10:53:00 -0800

    Changed a small detail in published feedback

--- app/views/Student/Profile.php
+++ app/views/Student/Profile.php
@@ -47,9 +47,7 @@
                   <div class="card mb-3">
                     <div class="card-body">
                       <div class="row">
-                        <div class="col-sm-3">
-                          <h6 class="mb-0" style="text-align: center;">Welcome to your profile</h6>
-                        </div>
+                        <h6 class="mb-0" >Welcome to your profile</h6>
                         
                       </div>
                     </div>
