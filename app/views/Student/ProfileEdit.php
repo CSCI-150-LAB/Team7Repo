3a645d2677d28f28168c1d4df5af9de08d0c6cea
sank27 <47614581+sank27@users.noreply.github.com>
2020-12-01 09:58:35 -0800

    Took out the display of range values

--- app/views/Student/ProfileEdit.php
+++ app/views/Student/ProfileEdit.php
@@ -9,17 +9,17 @@
    
     <br>
     <input type="range" name="visual" id="visual" min="1" max="10" value="<?php echo $profile->visual ?>">
-    <label for="visual">How often do you prefer visual learning tools?</label> <b><span id="visualvalue" style="color:red;"></span></b>
+    <label for="visual">How often do you prefer visual learning tools?</label> 
     
     <br>
     <input type="range" name="audio" id="audio" min="1" max="10" value="<?php echo $profile->audio ?>">
-    <label for="audio">How often do you prefer audio learning tools?</label> <b><span  id="audiovalue"style="color:red;"></span></b>
+    <label for="audio">How often do you prefer audio learning tools?</label> 
     <br>
     <input type="range" name="kinesthetic" id="kinesthetic" min="1" max="10" value="<?php echo $profile->kinesthetic ?>">
-    <label for="kinesthetic">How often do you prefer kinesthetic learning tools?</label> <b><span  id="kinestheticvalue" style="color:red;"></span></b>
+    <label for="kinesthetic">How often do you prefer kinesthetic learning tools?</label> 
     <br>
     <input type="range" name="readingwriting" id="readingwriting" min="1" max="10" value="<?php echo $profile->reading_writing ?>">
-    <label for="readingwriting">How often do you prefer reading/writing learning tools?</label> <b><span  id="readingwritingvalue" style="color:red;"></span></b>
+    <label for="readingwriting">How often do you prefer reading/writing learning tools?</label> 
     <br>
 
     
@@ -36,7 +36,7 @@
     <label for="audio-tools">Audio tools</label>
     <select name="audio-tools" id="audio-tools">
         <option value="Lecture">Lecture in person</option>
-        <option value="Storytelling">Recorded lectures / audio books </option>
+        <option value="Recorded-Lectures/Audio-Books">Recorded lectures / audio books </option>
     </select>
     <br>
 
