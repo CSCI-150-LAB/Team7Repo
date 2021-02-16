0f068ec3d20283c7a88604c36518c146b7d858ea
Daniel Flynn <daniel.n.flynn@gmail.com>
2021-02-16 15:20:41 -0800

    fix: floating button on chat form

--- app/vue-components/chat-form.vue
+++ app/vue-components/chat-form.vue
@@ -1,19 +1,19 @@
 <template>
 	<div class="messaging-form">
-		<div class = "row">
-			<div class = "col-4">
-				<div class = "card p-4">
-					<div class = "row">
-						<div class = "col-8">Recent Messages</div>
-						<div class = "col">
-							<button type = "button" class="btn btn-secondary pull-right" data-toggle = "tooltip" data-placement = "right" title = "Start New Conversation">
-								<i class = "fas fa-edit"></i>
+		<div class="row">
+			<div class="col-4">
+				<div class="card p-4">
+					<div class="row">
+						<div class="col-8">Recent Messages</div>
+						<div class="col">
+							<button type="button" class="btn btn-secondary float-right" data-toggle="tooltip" data-placement="right" title="Start New Conversation">
+								<i class="fas fa-edit"></i>
 							</button>
 						</div>
 					</div>
-					<div class = "row">
-						<div class = "col user-list">
-							<div v-for="user in userList" class = "row" v-bind:key="user.id">
+					<div class="row">
+						<div class="col user-list">
+							<div v-for="user in userList" class="row" v-bind:key="user.id">
 								<div> <img v-bind:src="baseUrl('public/images/blank_avatar.png')" width="50" height="50" alt="blank_avatar" class="mr-md-4 mb-3 img-fluid"> </div>
 								<div> {{user.fullName}} </div>
 							</div>
@@ -24,13 +24,13 @@
 
 			<!-- List of messages above, selected message group below -->
 
-			<div class = "col">
-				<div class = "card p-4">
+			<div class="col">
+				<div class="card p-4">
 					<div ref="log" class="message-log"></div>
 					<form class="messaging-form" v-on:submit="submit">
 						<div class="input-group">
-							<input type = "text" class = "form-control" v-model="messageTxt" placeholder = "Message">
-							<button type = "submit" class = "btn btn-primary" v-bind:disabled="!isSocketConnected"><i class="fas fa-search"></i></button>
+							<input type="text" class="form-control" v-model="messageTxt" placeholder="Message">
+							<button type="submit" class="btn btn-primary" v-bind:disabled="!isSocketConnected"><i class="fas fa-search"></i></button>
 						</div>
 					</form>
 				</div>
