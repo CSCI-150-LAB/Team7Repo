7f0d626813a0cf3ffbf4af25e188227ec656b24b
SumanjitGi11 <70239811+SumanjitGi11@users.noreply.github.com>
2021-02-18 17:54:18 -0800

    Student Profile Redirect Fix

--- app/views/Student/Profile.php
+++ app/views/Student/Profile.php
@@ -23,7 +23,7 @@
                       
                         <img src="<?php echo $this->publicUrl('images/blank_avatar.png')?>" alt="pic" class="rounded-circle" width="150">
                         <div class="mt-3">
-                          <h4><?php echo $currentUser->getFullName()?></h4>
+                          <h4><?php echo $user->getFullName()?></h4>
                           <p class="text-secondary mb-1"><?php echo $profile->studentMajor?></p>
                         </div>
                       </div>
@@ -33,7 +33,7 @@
                     <ul class="list-group list-group-flush">
                       <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                         <h6 class="mb-0">Email</h6>
-                        <span class="text-secondary"><?php echo $currentUser->email?></span>
+                        <span class="text-secondary"><?php echo $user->email?></span>
                       </li>
                       <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                         <h6 class="mb-0">Preferred Learning Style </h6>
