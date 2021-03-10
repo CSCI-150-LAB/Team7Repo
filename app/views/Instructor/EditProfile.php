f2bbedd343d5777cc28b186b88caff0ea9d93f74
Robert Wong <70239645+wongrob@users.noreply.github.com>
2021-03-09 17:39:09 -0800

    updated tours
    added template which does not fully work yet, added instructor edit profile tutorial

--- app/views/Instructor/EditProfile.php
+++ app/views/Instructor/EditProfile.php
@@ -3,9 +3,10 @@
 ?>
 
 <h1>Edit Your Profile</h1>
-<form method = 'POST'>
+<button class = 'btn btn-secondary float-md-right text-white' onclick = 'instrProfEditTut()'>Help</button><br><br>
+<form class = 'editinstructorprofile' method = 'POST'>
     <!--Once submitted, save to database by sending to controller-->
-    <div class="form-group">
+    <div class="form-group department">
 		<label for="department">Department:</label>
 		<input type="text" class="form-control <?php echo !empty($errors['class']) ? 'is-invalid' : '' ?>" id="department" name="department" placeholder="Math" <?php echo "value = '".$profile->department."'"; ?>>
 		<?php if (!empty($errors['department'])) : ?>
@@ -16,7 +17,7 @@
 	</div>
     <br>
     <!--Input for user's department-->
-	<div class="form-row">
+	<div class="form-row titlename">
 		<div class="form-group col-md-4">
 			<label>Preferred Title:</label>
 		</div>
@@ -64,165 +65,254 @@
 			</div>
 		</div>
 	</div>
-	<div class="form-row">
-		<div class="form-group col-md-4">
-			<label>Visual Style Usage:</label>
+	<div class="teachingstyles">
+		<div class="form-row visual">
+			<div class="form-group col-md-4">
+				<label>Visual Style Usage:</label>
+			</div>
+			<div class="form-group col-md-8">
+				<!--Selection of user's visual style usage, autoselects if previously chosen-->
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='visual1' name = 'visual' value = 'Primarily'
+						<?php   if($profile->visual == 'primarily') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='visual1'>Primarily</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='visual2' name = 'visual' value = 'Somewhat'
+						<?php   if($profile->visual == 'somewhat') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='visual2'>Somewhat</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='visual3' name = 'visual' value = 'Minimal'
+						<?php   if($profile->visual == 'minimal') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='visual3'>Minimal</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='visual4' name = 'visual' value = 'Not at all'
+						<?php   if($profile->visual == 'not at all') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='visual4'>Not at all</label>
+				</div>
+			</div>
 		</div>
-		<div class="form-group col-md-8">
-			<!--Selection of user's visual style usage, autoselects if previously chosen-->
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='visual1' name = 'visual' value = 'Primarily'
-					<?php   if($profile->visual == 'primarily') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='visual1'>Primarily</label>
+		<div class="form-row auditory">
+			<div class="form-group col-md-4">
+				<label>Auditory Style Usage:</label>
 			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='visual2' name = 'visual' value = 'Somewhat'
-					<?php   if($profile->visual == 'somewhat') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='visual2'>Somewhat</label>
+			<div class="form-group col-md-8">
+				<!--Selection of user's auditory style usage, autoselects if previously chosen-->
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='auditory1' name = 'auditory' value = 'Primarily'
+						<?php   if($profile->auditory == 'primarily') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='auditory1'>Primarily</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='auditory2' name = 'auditory' value = 'Somewhat'
+						<?php   if($profile->auditory == 'somewhat') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='auditory2'>Somewhat</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='auditory3' name = 'auditory' value = 'Minimal'
+						<?php   if($profile->auditory == 'minimal') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='auditory4'>Minimal</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='auditory4' name = 'auditory' value = 'Not at all'
+						<?php   if($profile->auditory == 'not at all') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='auditory3'>Not at all</label>
+				</div>
 			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='visual3' name = 'visual' value = 'Minimal'
-					<?php   if($profile->visual == 'minimal') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='visual3'>Minimal</label>
+		</div>
+		<div class="form-row readwrite">
+			<div class="form-group col-md-4">
+				<label>Reading and Writing Style Usage:</label>
 			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='visual4' name = 'visual' value = 'Not at all'
-					<?php   if($profile->visual == 'not at all') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='visual4'>Not at all</label>
+			<div class="form-group col-md-8">
+				<!--Selection of user's reading/writing style usage, autoselects if previously chosen-->
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='readwrite1' name = 'readwrite' value = 'Primarily'
+						<?php   if($profile->readwrite == 'primarily') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='readwrite1'>Primarily</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='readwrite2' name = 'readwrite' value = 'Somewhat'
+						<?php   if($profile->readwrite == 'somewhat') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='readwrite2'>Somewhat</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='readwrite3' name = 'readwrite' value = 'Minimal'
+						<?php   if($profile->readwrite == 'minimal') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='readwrite3'>Minimal</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='readwrite4' name = 'readwrite' value = 'Not at all'
+						<?php   if($profile->readwrite == 'not at all') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='readwrite4'>Not at all</label>
+				</div>
+			</div>
+		</div>
+		<div class="form-row kinesthetic">
+			<div class="form-group col-md-4">
+				<label>Kinesthetic Style Usage:</label>
+			</div>
+			<div class="form-group col-md-8">
+				<!--Selection of user's kinesthetic style usage, autoselects if previously chosen-->
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='kines' name = 'kines' value = 'Primarily'
+						<?php   if($profile->kines == 'primarily') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='kines1'>Primarily</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='kines2' name = 'kines' value = 'Somewhat'
+						<?php   if($profile->kines == 'somewhat') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='kines2'>Somewhat</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='kines3' name = 'kines' value = 'Minimal'
+						<?php   if($profile->kines == 'minimal') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='kines3'>Minimal</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" id='kines4' name = 'kines' value = 'Not at all'
+						<?php   if($profile->kines == 'not at all') {
+							echo "checked = 'checked'";
+						} ?>
+				>
+					<label class="form-check-label" for='kines4'>Not at all</label>
+				</div>
 			</div>
 		</div>
 	</div>
-	<div class="form-row">
-		<div class="form-group col-md-4">
-			<label>Auditory Style Usage:</label>
-		</div>
-		<div class="form-group col-md-8">
-			<!--Selection of user's auditory style usage, autoselects if previously chosen-->
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='auditory1' name = 'auditory' value = 'Primarily'
-					<?php   if($profile->auditory == 'primarily') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='auditory1'>Primarily</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='auditory2' name = 'auditory' value = 'Somewhat'
-					<?php   if($profile->auditory == 'somewhat') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='auditory2'>Somewhat</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='auditory3' name = 'auditory' value = 'Minimal'
-					<?php   if($profile->auditory == 'minimal') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='auditory4'>Minimal</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='auditory4' name = 'auditory' value = 'Not at all'
-					<?php   if($profile->auditory == 'not at all') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='auditory3'>Not at all</label>
-			</div>
-		</div>
-	</div>
-	<div class="form-row">
-		<div class="form-group col-md-4">
-			<label>Reading and Writing Style Usage:</label>
-		</div>
-		<div class="form-group col-md-8">
-			<!--Selection of user's reading/writing style usage, autoselects if previously chosen-->
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='readwrite1' name = 'readwrite' value = 'Primarily'
-					<?php   if($profile->readwrite == 'primarily') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='readwrite1'>Primarily</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='readwrite2' name = 'readwrite' value = 'Somewhat'
-					<?php   if($profile->readwrite == 'somewhat') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='readwrite2'>Somewhat</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='readwrite3' name = 'readwrite' value = 'Minimal'
-					<?php   if($profile->readwrite == 'minimal') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='readwrite3'>Minimal</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='readwrite4' name = 'readwrite' value = 'Not at all'
-					<?php   if($profile->readwrite == 'not at all') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='readwrite4'>Not at all</label>
-			</div>
-		</div>
-	</div>
-	<div class="form-row">
-		<div class="form-group col-md-4">
-			<label>Kinesthetic Style Usage:</label>
-		</div>
-		<div class="form-group col-md-8">
-			<!--Selection of user's kinesthetic style usage, autoselects if previously chosen-->
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='kines' name = 'kines' value = 'Primarily'
-					<?php   if($profile->kines == 'primarily') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='kines1'>Primarily</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='kines2' name = 'kines' value = 'Somewhat'
-					<?php   if($profile->kines == 'somewhat') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='kines2'>Somewhat</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='kines3' name = 'kines' value = 'Minimal'
-					<?php   if($profile->kines == 'minimal') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='kines3'>Minimal</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" id='kines4' name = 'kines' value = 'Not at all'
-					<?php   if($profile->kines == 'not at all') {
-						echo "checked = 'checked'";
-					} ?>
-			>
-				<label class="form-check-label" for='kines4'>Not at all</label>
-			</div>
-		</div>
-	</div>
-	<input type = 'submit' class = 'btn btn-primary' name = 'save changes' value = 'Save Changes'>
+	<input type = 'submit' class = 'btn btn-primary savebutton' name = 'save changes' value = 'Save Changes'>
 </form> <!--Submit button-->
+
+<script>
+	function instrProfEditTut(){
+		// Declare the tour
+		var instructorProfileEditTutorial = new Tour({
+			name: "instructor edit profile tour",
+			container: "body",
+			smartPlacement: true,
+			backdrop: true,
+			backdropPadding: 5,
+			duration: 10000,
+			storage: false,
+			template: '<div class="popover" role="tooltip"> <div class="arrow"></div> <h3 class="popover-header"></h3> <div class="popover-body"></div> <div class="popover-navigation"> <div class="btn-group"> <button class="btn btn-sm btn-secondary" data-role="prev"><< Prev</button> <button class="btn btn-sm btn-secondary" data-role="next">Next >></button> <button class="btn btn-sm btn-secondary" data-role="pause-resume" data-pause-text="Pause" data-resume-text="Resume">Pause</button> </div> <button class="btn btn-sm btn-secondary" data-role="end">End tour</button> </div> </div>',
+            steps: [
+            {
+                element: ".editinstructorprofile",
+                title: "Edit your profile here!",
+				next: 1,
+				prev:-1,
+                content: "This is where you can first create, edit, or update your instructor profile."
+            },
+            {
+                element: ".department",
+                title: "Change your department",
+				next: 2,
+				prev: 0,
+                content: "Here is where you can type in the name of the department you work for, you can use an abbreviation or type it out fully, whatever you decide."
+            },
+            {
+                element: ".titlename",
+                title: "Preferred Title",
+				next: 3,
+				prev: 1,
+                content: "Select your preferred title for how you are addressed.  You can only select one, but if you change your mind you can always come back and change it later."
+            },
+			{
+				element: ".teachingstyles",
+				title: "Select Teaching Styles",
+				next: 4,
+				prev: 2,
+				content: "Here is where you select your usage of each teaching style.  We recommend you be as accurate as possible so students have a better idea of how your class is run."
+			},
+			{
+				element: ".visual",
+				title: "Visual Teaching",
+				next: 5,
+				prev: 3,
+				content: "Visual teaching broadly refers to any teaching that uses visuals.  Commonly this includes lecture slides, videos, and other visual aids.  Select how often you believe you use these types of materials when teaching here."
+			},
+			{
+				element: ".auditory",
+				title: "Auditory Teaching",
+				next: 6,
+				prev: 4,
+				content: "Auditory teaching broadly refers to any teaching that uses sound.  Commonly this includes lectures, videos, music, and other things you listen to.  Select how often you believe you use these types of materials when teaching here."
+			},
+			{
+				element: ".readwrite",
+				title: "Reading and Writing Teaching",
+				next: 7,
+				prev: 5,
+				content: "Reading and writing teaching broadly refers to any teaching where students can read and write while learning.  Commonly this includes when students take notes, but can also refer to writing essays, responses to quizzes, assigning textbook reading, and so on.  Select how often you believe you use these types of materials when teaching here."
+			},
+			{
+				element: ".kinesthetic",
+				title: "Kinesthetic Teaching",
+				next: 8,
+				prev: 6,
+				content: "Kinesthetic teaching broadly refers to any teaching that encourages students to participate.  Commonly this includes homework assignments, quizzes, labs, and other activities where students get a hands-on experience.  Select how often you believe you use these types of materials when teaching here"
+			},
+			{
+				element: ".savebutton",
+				title: "Submit",
+				next: -1,
+				prev: 7,
+				content: "When you are done with editting your profile, you can click here to save it and it will redirect you back to your profile to view."
+			}]
+        });
+	  
+		// Initialize the tour
+		instructorProfileEditTutorial.init();
+	  
+		// Start the tour
+		instructorProfileEditTutorial.restart();
+	  
+	};
+</script>
--- app/views/Instructor/Profile.php
+++ app/views/Instructor/Profile.php
@@ -9,8 +9,9 @@ $this->pageTitle("{$user->preferredTitle} {$user->lastName} - Profile");
 <div class="bg-blue p-5 text-white mb-3">
 	<h1 class="mb-0">Instructor Profile</h1>
 </div>
-<?php if(($profile->instructorid == $currentUser->id) || ($currentUser->type == "admin")) {
-	echo "<a class = 'btn btn-secondary float-md-right text-white' onclick = 'instrProfTut()')>Help</a><br><br>";
+<button class = 'btn btn-secondary float-md-right text-white' onclick = 'instrProfTut()'>Help</button><br><br> 
+<?php
+	if(($profile->instructorid == $currentUser->id) || ($currentUser->type == "admin")) {
 	echo "<a class = 'btn btn-secondary float-md-right text-white editprofile' href = '".$this->baseUrl("/Instructor/EditProfile/{$currentUser->id}")."'>Edit Profile</a><br><br>";
 } //Allows user to edit profile if current profile is the user's profile ?>
 <div class="d-flex flex-column flex-md-row instructorinfo">
@@ -191,41 +192,42 @@ $this->pageTitle("{$user->preferredTitle} {$user->lastName} - Profile");
 			backdropPadding: 5,
 			duration: 10000,
 			storage: false,
+			template: '<div class="popover" role="tooltip"> <div class="arrow"></div> <h3 class="popover-header"></h3> <div class="popover-body"></div> <div class="popover-navigation"> <div class="btn-group"> <button class="btn btn-sm btn-secondary" data-role="prev"><< Prev</button> <button class="btn btn-sm btn-secondary" data-role="next">Next >></button> <button class="btn btn-sm btn-secondary" data-role="pause-resume" data-pause-text="Pause" data-resume-text="Resume">Pause</button> </div> <button class="btn btn-sm btn-secondary" data-role="end">End tour</button> </div> </div>',
             steps: [
             {
                 element: ".instructorinfo",
-                title: "Your Profile",
+                title: "Instructor Profile",
 				next: 1,
 				prev:-1,
-                content: "This is your instructor profile, here is basic information about you."
+                content: "This is an instructor profile, here is basic information about the instructor."
             },
             {
                 element: ".editprofile",
                 title: "Edit Profile",
 				next: 2,
 				prev: 0,
-                content: "You can update that information by pressing here."
+                content: "You can update that information by pressing here, or you can continue this tour first and update your information later."
             },
             {
                 element: ".starrating",
                 title: "Rating",
 				next: 3,
 				prev: 1,
-                content: "Here is your overall instructor rating based on all your reviews.  You can hover your mouse over it to see the fractional value."
+                content: "Here is this instructor's overall instructor rating based on all of their reviews.  You can hover your mouse over it to see the fractional value."
             },
 			{
 				element: ".teachingstyles",
-				title: "Your selected Teaching Styles",
+				title: "Selected Teaching Styles",
 				next: 4,
 				prev: 2,
-				content: "Here are the selected options for how frequently you use each of the four primary learning/teaching styles."
+				content: "Here are the selected options for how frequently this instructor uses each of the four primary learning/teaching styles."
 			},
 			{
 				element: ".topreview",
 				title: "Top Review",
 				next: 5,
 				prev: 3,
-				content: "This is the top review for you based on the highest rating, and by a verified student if you have a review by a verified student."
+				content: "This is the top review for this instructor based on the highest rating, and by a verified student if there is a review by a verified student."
 			},
 			{
 				element: ".studentrating:first",
@@ -239,42 +241,42 @@ $this->pageTitle("{$user->preferredTitle} {$user->lastName} - Profile");
 				title: "Class Information",
 				next: 7,
 				prev: 5,
-				content: "Here is where students can leave optional information about their class.  Options include if they would take the class again, if there was a lot of homework in the class, whether or not class attendance is required, and their self-reported grade."
+				content: "Here is where students can leave optional information about the class they took with this instructor.  Options include if they would take the class again, if there was a lot of homework in the class, whether or not class attendance is required, and their self-reported grade."
 			},
 			{
 				element: ".reviewinfo:first",
 				title: "Review Description",
 				next: 8,
 				prev: 6,
-				content: "This is where the student left personalized information about how they felt about the class."
+				content: "This is where the student left personalized information about how they felt about the class or the instructor."
 			},
 			{
 				element: ".author:first",
 				title: "Reviewer",
 				next: 9,
 				prev: 7,
-				content: "This is where the name of the student that left the review is if they decided not to be anonymous.  The green checkmark next to their name means they have been verified to have been in one of your classes."
+				content: "This is where the name of the student that left the review is if they decided not to be anonymous.  The green checkmark next to their name means they have been verified to have been in one of the instructor's classes."
 			},
 			{
 				element: ".recentfeedback",
 				title: "Recent Feedback",
 				next: 10,
 				prev: 8,
-				content: "Here is where you will find up to the two most recent feedback for you."
+				content: "Here is where you will find up to the two most recent feedback for this instructor."
 			},
 			{
 				element: ".allreviews",
 				title: "See all reviews",
 				next: 11,
 				prev: 9,
-				content: "You can click here to view all of the reviews you have received."
+				content: "You can click here to view all of the reviews this instructor has received."
 			},
 			{
 				element: ".takeagain",
 				title: "Who would take again?",
 				next: 12,
 				prev: 10,
-				content: "If students have answered the optional question about whether they would take your class again, the percentage who responded yes out of those who responded will be shown here."
+				content: "If students have answered the optional question about whether they would take this instructor's class again, the percentage who responded yes out of those who responded will be shown here."
 			},
 			{
 				element: ".grades",
