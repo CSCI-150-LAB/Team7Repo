4a770fdf68d3da345c8da3940b7a9ee229ef1859
Robert Wong <70239645+wongrob@users.noreply.github.com>
2021-04-15 18:46:09 -0700

    removed UI for non-existent features

--- app/views/Admin/AdminAccounts.php
+++ app/views/Admin/AdminAccounts.php
@@ -2,7 +2,7 @@
     <h1 class="mb-3" style= "background-color: #13284c; padding:60px; color: #ffffff;">Admin Accounts</h1>
         
         <a class = "btn btn-secondary float-right" style="color: #ffffff;" href ='<?php echo $this->baseUrl("/Admin/AddUser") ?>'>Add an Admin</a><br><br>
-        <a class = "btn btn-secondary float-right" style="color: #ffffff;" href ='<?php echo $this->baseUrl("/Admin/AddUser") ?>'>Add Admin by CSV</a><br><br>
+        <!-- <a class = "btn btn-secondary float-right" style="color: #ffffff;" href ='<?php echo $this->baseUrl("/Admin/AddUser") ?>'>Add Admin by CSV</a><br><br> -->
     
     <table class="table table-bordered table-sm table-striped pl-5 tbl-background"> 
         <thead>
