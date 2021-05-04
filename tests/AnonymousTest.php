2b4447ccb70f4faa42207eb5bbaf8c31686cedcb
Daniel Flynn <daniel.n.flynn@gmail.com>
2021-05-03 20:12:40 -0700

    feat: switched test to user curl/curl

--- tests/AnonymousTest.php
+++ tests/AnonymousTest.php
@@ -1,15 +1,21 @@
 <?php declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use \Curl\Curl;
 
-require_once(__DIR__ . '/helpers/Config.php');
+require_once __DIR__ . '/../app/lib/vendor/autoload.php';
+require_once __DIR__ . '/helpers/Config.php';
 
 final class AnonymousTest extends TestCase
 {
-    public function testHomepage(): void
-    {
+	public function testHomepage(): void
+	{
 		$url = getConfig('siteUrl');
-		$html = file_get_contents("{$url}");
-		$this->assertStringContainsString('<h2>Welcome to FeedbackLoop!</h2>', $html, 'Welcome message exists');
-    }
+
+		$req = new Curl();
+		// $req->setOpt(CURLOPT_FOLLOWLOCATION, true);
+		$req->get($url);
+
+		$this->assertStringContainsString('<h2>Welcome to FeedbackLoop!</h2>', $req->response, 'Welcome message exists');
+	}
 }
