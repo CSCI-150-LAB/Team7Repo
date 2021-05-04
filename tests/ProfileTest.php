c512077039ef59b0b32ca2c9ea8e17c3d6c33973
Robert Wong <70239645+wongrob@users.noreply.github.com>
2021-05-03 21:28:42 -0700

    Update ProfileTest.php

--- tests/ProfileTest.php
+++ tests/ProfileTest.php
@@ -1,29 +1,43 @@
 <?php declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use \Curl\Curl;
 
-require_once(__DIR__ . '/helpers/Config.php');
+require_once __DIR__ . '/../app/lib/vendor/autoload.php';
+require_once __DIR__ . '/helpers/Config.php';
 
 final class ProfileTest extends TestCase
 {
     public function testinstructorFeedbackNone(): void
-    {
-		$url = getConfig('siteUrl');
-		$html = file_get_contents("{$url}Instructor/Profile/1454");
-		$this->assertStringContainsString('No recommendations yet', $html, 'Alternative when there are no recommendations appears');
-    }
+	  {
+	  	$url = getConfig('siteUrl');
+
+	  	$req = new Curl();
+	  	// $req->setOpt(CURLOPT_FOLLOWLOCATION, true);
+	  	$req->get("{$url}Instructor/Profile/1454");
+
+	  	$this->assertStringContainsString('No recommendations yet', $req->response, 'Alternative when there are no recommendations appears');
+	  }
 
     public function testinstructorFeedbackExists(): void
-    {
-		$url = getConfig('siteUrl');
-		$html = file_get_contents("{$url}Instructor/Profile/1443");
-		$this->assertStringContainsString('<div class="author">', $html, 'Recommendation appears on page');
-    }
+	  {
+	  	$url = getConfig('siteUrl');
+
+	  	$req = new Curl();
+	  	// $req->setOpt(CURLOPT_FOLLOWLOCATION, true);
+	  	$req->get("{$url}Instructor/Profile/1443");
+
+	  	$this->assertStringContainsString('<div class="author">', $req->response, 'Recommendation appears on page');
+	  }
 
     public function testinstructorFeedbackVerified(): void
-    {
-		$url = getConfig('siteUrl');
-		$html = file_get_contents("{$url}Instructor/Profile/1443");
-		$this->assertStringContainsString('<i class="fas fa-check ml-1 text-success"></i>', $html, 'Verified checkmark appears');
-    }
+	  {
+	  	$url = getConfig('siteUrl');
+
+	  	$req = new Curl();
+	  	// $req->setOpt(CURLOPT_FOLLOWLOCATION, true);
+	  	$req->get("{$url}Instructor/Profile/1443");
+
+	  	$this->assertStringContainsString('<i class="fas fa-check ml-1 text-success"></i>', $req->response, 'Verified checkmark appears');
+	  }
 }
